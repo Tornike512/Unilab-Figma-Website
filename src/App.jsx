23688762ac83eac9b90c3c006c8ae5eec7d8086d
Tornike512 <Dht5152@gmail.com>
2024-04-05 21:34:03 +0400

    Start: Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import { PublicLayout } from "./layouts/PublicLayout";
 import { Header } from "./components/Header";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Router>
+      <>
+        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<PublicLayout />} />
+          </Routes>
+        </Suspense>
+      </>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,5 @@
+import "./HomePage.scss";
+
+export function HomePage() {
+  return <div></div>;
+}
