da5942be53c6ee21a7bec2955c68aeb75484b2b6
Tornike512 <Dht5152@gmail.com>
2024-04-06 21:59:45 +0400

    Add: Jsconfig And Footer Socials CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { PublicLayout } from "./layouts/PublicLayout";
-import { Header } from "./components/Header";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 
 function App() {
   return (
     <Router>
-      <>
-        <Header />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route element={<PublicLayout />} />
-          </Routes>
-        </Suspense>
-      </>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,47 @@
 import "./Footer.scss";
 
+import instagramLogo from "/src/assets/instagram-logo.png";
+import facebookLogo from "/src/assets/facebook-logo.png";
+import linkedinLogo from "/src/assets/linkedin-logo.png";
+
 export function Footer() {
   return (
     <footer className="footer">
-      <div className="socials">
-        <title>ExploreEra</title>
-        <p>Travel agency which helps you to plan your perfect holidays</p>
-        <span>Follow us</span>
-        <nav className="socials-image">
-          <img src="" alt="Instagram Logo" />
-          <img src="" alt="Facebook Logo" />
-          <img src="" alt="Linkedin Logo" />
+      <div className="footer-wrapper">
+        <div className="socials">
+          <h2>ExploreEra</h2>
+          <p>Travel agency which helps you to plan your perfect holidays</p>
+          <span>Follow us</span>
+          <nav className="socials-image">
+            <img src={instagramLogo} alt="Instagram Logo" />
+            <img src={facebookLogo} alt="Facebook Logo" />
+            <img src={linkedinLogo} alt="Linkedin Logo" />
+          </nav>
+        </div>
+        <nav className="links">
+          <span>Links</span>
+          <a href="#">Home</a>
+          <a href="#">About us</a>
+          <a href="#">Blogs</a>
+          <a href="#">Our services</a>
+          <a href="#">Our offer</a>
         </nav>
-      </div>
-      <nav className="links">
-        <span>Links</span>
-        <a href="#">Home</a>
-        <a href="#">About us</a>
-        <a href="#">Blogs</a>
-        <a href="#">Our services</a>
-        <a href="#">Our offer</a>
-      </nav>
-      <div className="contact">
-        <h3>Contact us</h3>
-        <span className="email">exploreera@gmail.com</span>
-        <span className="phone-number">555111222</span>
-        <label className="subscribe">Subscribe news</label>
-        <form action="">
-          <input type="email" placeholder="Email" />
-          <img
-            src=""
-            alt="Send Arrow Logo
+        <div className="contact">
+          <h3>Contact us</h3>
+          <span className="email">exploreera@gmail.com</span>
+          <span className="phone-number">555111222</span>
+          <label className="subscribe">Subscribe news</label>
+          <form action="">
+            <input type="email" placeholder="Email" />
+            <img
+              src=""
+              alt="Send Arrow Logo
           "
-          />
-        </form>
+            />
+          </form>
+        </div>
       </div>
+      <span className="copyright">Copyright 2024</span>
     </footer>
   );
 }
