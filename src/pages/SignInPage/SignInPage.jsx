0c648f721027c59ed856c79e7fd5c466f643c419
Tornike512 <Dht5152@gmail.com>
2024-04-07 19:58:38 +0400

    Add: Sign In Page

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,20 +1,41 @@
+import coloredFacebook from "/src/assets/colored-facebook.png";
+import coloredApple from "/src/assets/colored-apple.png";
+import coloredGmail from "/src/assets/colored-gmail.png";
+import coloredGoogle from "/src/assets/colored-google.png";
+
 import "./SignInPage.scss";
 
 export function SignInPage() {
   return (
     <section className="sign-in-page">
       <div className="sign-in-container">
-        <label>Email</label>
-        <input type="email" placeholder="Enter your email address" />
-        <label>Password</label>
-        <input type="password" placeholder="Enter your password" />
+        <div className="email-input">
+          <label>Email</label>
+          <input type="email" placeholder="Enter your email address" />
+        </div>
+        <div className="password-input">
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password" />
+        </div>
         <button className="sign-in-button">Countinue</button>
-        <span className="divider-text">Or continue with</span>
+        <div className="divider-text">Or continue with</div>
         <div className="upper-divider"></div>
-        <div className="sign-in-with-socials"></div>
-        <div className="lower-divider"></div>
-        <p>
-          By signing in or creating an account, you agree with our
+        <div className="sign-in-socials-grid">
+          <div className="home-page-socials">
+            <img src={coloredFacebook} alt="Colored Facebook Icon" />
+          </div>
+          <div className="home-page-socials">
+            <img src={coloredApple} alt="Colored apple Icon" />
+          </div>
+          <div className="home-page-socials">
+            <img src={coloredGoogle} alt="Colored Google Icon" />
+          </div>
+          <div className="home-page-socials">
+            <img src={coloredGmail} alt="Colored Gmail Icon" />
+          </div>
+        </div>
+        <p className="terms-of-service">
+          By signing in or creating an account, you agree with our <br />
           <a href="#">Terms & conditions</a> and <a href="#">Privacy policy</a>
         </p>
       </div>
