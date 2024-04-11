e9c8aff5fbec0856b30312508aba5b345be2c3ff
Tornike512 <Dht5152@gmail.com>
2024-04-11 22:22:52 +0400

    Add: Sign In Page Functions For Signing In

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -12,25 +12,79 @@ export function SignInPage() {
   const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
 
-  const storedEmail = localStorage.getItem("email");
-  const storedPassword = localStorage.getItem("password");
+  const [emailWarning, setEmailWarning] = useState(false);
+  const [passwordWarning, setPasswordWarning] = useState(false);
 
-  console.log(storedEmail);
-  console.log(storedPassword);
+  const storedEmail = JSON.parse(localStorage.getItem("email"));
+  const storedPassword = JSON.parse(localStorage.getItem("password"));
+
+  const navigate = useNavigate();
+
+  const handleEmailInput = (e) => {
+    setEmailInput(e.target.value);
+    setEmailWarning(false);
+  };
+
+  const handlePasswordInput = (e) => {
+    setPasswordInput(e.target.value);
+    setPasswordWarning(false);
+  };
+
+  const handleSignInButton = () => {
+    if (emailInput === storedEmail && passwordInput === storedPassword) {
+      navigate("/bookflight");
+    }
+
+    if (emailInput !== storedEmail) {
+      setEmailWarning(true);
+    }
+
+    if (passwordInput !== storedPassword) {
+      setPasswordWarning(true);
+    }
+  };
 
   return (
     <section className="sign-in-page">
       <div className="sign-in-container">
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <div className="email-input">
             <label>Email</label>
-            <input type="email" placeholder="Enter your email address" />
+            <input
+              style={
+                emailWarning
+                  ? {
+                      border: "1px solid red",
+                      boxShadow: "inset 0 0 2px red",
+                    }
+                  : {}
+              }
+              onChange={handleEmailInput}
+              type="email"
+              placeholder="Enter your email address"
+            />
+            {emailWarning && <span className="warning">Wrong password</span>}
           </div>
           <div className="password-input">
             <label>Password</label>
-            <input type="password" placeholder="Enter your password" />
+            <input
+              style={
+                passwordWarning
+                  ? {
+                      border: "1px solid red",
+                      boxShadow: "inset 0 0 2px red",
+                    }
+                  : {}
+              }
+              onChange={handlePasswordInput}
+              type="password"
+              placeholder="Enter your password"
+            />
+            {passwordWarning && <span className="warning">Wrong password</span>}
           </div>
-          <button className="sign-in-button">Countinue</button>
+          <button onClick={handleSignInButton} className="sign-in-button">
+            Countinue
+          </button>
         </form>
         <div className="divider-text">Or continue with</div>
         <div className="upper-divider"></div>
