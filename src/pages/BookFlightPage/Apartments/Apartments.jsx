ce9ab0f7f2ee5837d55c28abf741c1c50611fb45
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-05 22:24:22 +0400

    Add: Apartment Info Section HTML

--- src/pages/BookFlightPage/Apartments/Apartments.jsx
+++ src/pages/BookFlightPage/Apartments/Apartments.jsx
@@ -1,5 +1,43 @@
 import React from "react";
 
 export default function Apartments() {
-  return <div></div>;
+  return (
+    <section className="apartment-section">
+      <aside className="apartment-info">
+        <li className="checkout-info">
+          <h2>Searched by:</h2>
+          <ul>
+            <label>Destination</label>
+            <input placeholder="Auckland, New Zealand" />
+          </ul>
+          <ul>
+            <label>Check-in date</label>
+            <input placeholder="Thursday, February 1, 2024" />
+          </ul>
+          <ul>
+            <label>Check-out date</label>
+            <input placeholder="Friday, February 9, 2024" />
+          </ul>
+          <ul>
+            <label>Persons</label>
+            <input placeholder="2 Adults, 0 children, 1 room" />
+            <label>Total 8 days</label>
+          </ul>
+        </li>
+        <img src="" alt="Apartament On Map" />
+        <li className="facilities">
+          <h4>All the facilities:</h4>
+          <ul>Private pool</ul>
+          <ul>Terrace</ul>
+          <ul>Kitchen</ul>
+          <ul>1 Bedroom</ul>
+          <ul>2 Bathrooms</ul>
+          <ul>kids play space</ul>
+          <ul>Billiard room</ul>
+          <ul>Private gym</ul>
+        </li>
+      </aside>
+      <div className="apartment"></div>
+    </section>
+  );
 }
