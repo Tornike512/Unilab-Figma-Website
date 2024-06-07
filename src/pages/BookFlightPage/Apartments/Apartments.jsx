808fccb2eb8f52b56c1f8ce4e76e3f351245e8b3
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-07 22:04:59 +0400

    Add: Checkout Info Style

--- src/pages/BookFlightPage/Apartments/Apartments.jsx
+++ src/pages/BookFlightPage/Apartments/Apartments.jsx
@@ -1,41 +1,42 @@
 import React from "react";
+import "./Apartments.scss";
 
 export default function Apartments() {
   return (
     <section className="apartment-section">
       <aside className="apartment-info">
-        <li className="checkout-info">
+        <ul className="checkout-info">
           <h2>Searched by:</h2>
-          <ul>
+          <li>
             <label>Destination</label>
             <input placeholder="Auckland, New Zealand" />
-          </ul>
-          <ul>
+          </li>
+          <li>
             <label>Check-in date</label>
             <input placeholder="Thursday, February 1, 2024" />
-          </ul>
-          <ul>
+          </li>
+          <li>
             <label>Check-out date</label>
             <input placeholder="Friday, February 9, 2024" />
-          </ul>
-          <ul>
+          </li>
+          <li>
             <label>Persons</label>
             <input placeholder="2 Adults, 0 children, 1 room" />
             <label>Total 8 days</label>
-          </ul>
-        </li>
+          </li>
+        </ul>
         <img src="" alt="Apartment On Map" />
-        <li className="facilities">
+        <ul className="facilities">
           <h4>All the facilities:</h4>
-          <ul>Private pool</ul>
-          <ul>Terrace</ul>
-          <ul>Kitchen</ul>
-          <ul>1 Bedroom</ul>
-          <ul>2 Bathrooms</ul>
-          <ul>kids play space</ul>
-          <ul>Billiard room</ul>
-          <ul>Private gym</ul>
-        </li>
+          <li>Private pool</li>
+          <li>Terrace</li>
+          <li>Kitchen</li>
+          <li>1 Bedroom</li>
+          <li>2 Bathrooms</li>
+          <li>kids play space</li>
+          <li>Billiard room</li>
+          <li>Private gym</li>
+        </ul>
       </aside>
       <div className="apartment">
         <div className="apartment-title">
